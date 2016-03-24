f2e4b8433437ba74382b20c2b72d9ec69b5839b7
Vlad Golubev <golubev.vld@gmail.com>
2016-03-24 13:46:46 +0200

    Refactor server functions

--- server/oauth-service.js
+++ server/oauth-service.js
@@ -1,7 +1,7 @@
 DropboxOAuth = {};
 
 OAuth.registerService('dropbox', 2, null, function (query) {
-    var response = getTokens(query);
+    var response = getTokens(query.code);
     var accessToken = response.accessToken;
     var accountInfo = getAccountInfo(accessToken);
 
@@ -25,28 +25,29 @@ OAuth.registerService('dropbox', 2, null, function (query) {
     };
 });
 
-// returns an object containing:
-// - accessToken
-// - expiresIn: lifetime of token in seconds
-var getTokens = function (query) {
-    var config = ServiceConfiguration.configurations.findOne({ service: 'dropbox' });
-    if (!config)
-        throw new ServiceConfiguration.ConfigError();
-
+/**
+ * Exchange OAuth code to access tokens
+ * @param {String} code OAuth grant code
+ * @returns {{accessToken: {String}, expiresIn: *}}
+ */
+var getTokens = function (code) {
     var response;
+    var config = ServiceConfiguration.configurations.findOne({ service: 'dropbox' });
+    if (!config) throw new ServiceConfiguration.ConfigError();
+
     try {
         var redirectUri = OAuth._redirectUri('dropbox', config, {}, { secure: true }).replace('?close', '?close=true');
-        console.log(redirectUri);
-        response = HTTP.post(
-            'https://api.dropbox.com/1/oauth2/token', {
-                params: {
-                    code: query.code,
-                    client_id: config.clientId,
-                    client_secret: OAuth.openSecret(config.secret),
-                    redirect_uri: redirectUri,
-                    grant_type: 'authorization_code'
-                }
-            });
+        var hostname = 'https://api.dropbox.com/1/oauth2/token';
+        var options = {
+            params: {
+                code: code,
+                client_id: config.clientId,
+                client_secret: OAuth.openSecret(config.secret),
+                redirect_uri: redirectUri,
+                grant_type: 'authorization_code'
+            }
+        };
+        response = HTTP.post(hostname, options);
     } catch (err) {
         throw _.extend(new Error('Failed to complete OAuth handshake with Dropbox. ' + err.message),
             { response: err.response });
@@ -62,6 +63,11 @@ var getTokens = function (query) {
     }
 };
 
+/**
+ * Fetch the Dropbox account info
+ * @param {String} accessToken Dropbox valid access token
+ * @returns {Object} https://www.dropbox.com/developers/documentation/http/documentation#users-get_current_account
+ */
 var getAccountInfo = function (accessToken) {
     var hostname = 'https://api.dropboxapi.com/2/users/get_current_account';
     var options = {
